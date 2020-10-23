c3373256f901358e3be04b7fddd4c1c1633f6304
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-23 14:01:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,7 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each {|word| p "Hello #{word}!"}
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each_with_index {|word,idx| puts "#{idx+1}. #{word}"}
 end
